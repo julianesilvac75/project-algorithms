f7c85abbcaea6d8afbe8f69a1eac8018163808d7
Juliane <julianesilvac75@gmail.com>
2023-01-06 15:46:41 +0000

    test: encrypt_message function tests

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "testmessage"
+
+    assert encrypt_message(message, -1) == "egassemtset"
+    assert encrypt_message(message, 3) == "set_egassemt"
+    assert encrypt_message(message, 4) == "egassem_tset"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, "key")
+    
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1234, 10)
