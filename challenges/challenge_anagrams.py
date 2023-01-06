01845f48575f715d7232d05d539730b81ec1f4e1
Juliane <julianesilvac75@gmail.com>
2023-01-06 17:07:12 +0000

    linter: fix errors

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,35 +1,37 @@
 def merge_sort(list, start=0, end=None):
     if end is None:
         end = len(list)
-    if (end - start) > 1: # se não reduzi o suficiente, continua
-        mid = (start + end) // 2 # encontrando o meio
-        merge_sort(list, start, mid) # dividindo as listas
+    if (end - start) > 1:
+        mid = (start + end) // 2
+        merge_sort(list, start, mid)
         merge_sort(list, mid, end)
-        merge(list, start, mid, end) # unindo as listas
+        merge(list, start, mid, end)
 
     return list
 
+
 def merge(list, start, mid, end):
-    left = list[start:mid] # indexando a lista da esquerda
-    right = list[mid:end] # indexando a lista da direita
+    left = list[start:mid]
+    right = list[mid:end]
 
-    left_index, right_index = 0, 0 # as duas listas começarão do início
+    left_index, right_index = 0, 0
 
-    for general_index in range(start, end): # percorrer sobre a lista inteira como se fosse uma
-        if left_index >= len(left): # se os elementos da esquerda acabaram, preenche o restante com a lista da direita 
+    for general_index in range(start, end):
+        if left_index >= len(left):
             list[general_index] = right[right_index]
             right_index = right_index + 1
-        elif right_index >= len(right): # se os elementos da direita acabaram, preenche o restante com a lista da esquerda
+        elif right_index >= len(right):
             list[general_index] = left[left_index]
             left_index = left_index + 1
-        elif left[left_index] < right[right_index]: # se o elemento do topo da esquerda for menor que o da direita, ele será o escolhido
+        elif left[left_index] < right[right_index]:
             list[general_index] = left[left_index]
             left_index = left_index + 1
         else:
-            list[general_index] = right[right_index] # caso o da direita seja menor, ele será o escolhido
+            list[general_index] = right[right_index]
             right_index = right_index + 1
 
 # REFERENCIA: COURSE TRYBE
+
 
 def is_anagram(first_string, second_string):
     
@@ -41,10 +43,10 @@ def is_anagram(first_string, second_string):
 
     if first_string == "" or second_string == "":
         return (
-        "".join(string_1_sorted),
-        "".join(string_2_sorted),
-        False
-    )
+            "".join(string_1_sorted),
+            "".join(string_2_sorted),
+            False
+        )
     
     return (
         "".join(string_1_sorted),
