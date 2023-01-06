5542e4ece93ba47a7e16554bcd9b505a4a7498e5
Juliane <julianesilvac75@gmail.com>
2023-01-06 17:36:01 +0000

    feat: find duplicate function implemented

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums=None):
+    if (
+        nums is None or
+        type(nums) == str
+    ):
+        return False
+
+    nums.sort()
+    for i in range(len(nums) - 1):
+        if nums[i] == nums [i + 1] and nums[1] > 0:
+            return nums[i]
+
+    return False
